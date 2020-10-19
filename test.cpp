3983cddd8b3038a22afba8ce5a41e079aaa83ca5
anastas203096 <72856675+anastas203096@users.noreply.github.com>
2020-10-19 21:33:35 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,12 +7,17 @@ bool unitTest() {
     Calculator c;
 
    std::cout << "-Check sum " << std::endl;
-    if (c.Calculate(10, '+', 10) != 20) return false;
+        if (c.Calculate(10, '+', 10) != 20) return false;
    std::cout << "-Check substraction " << std::endl;
-    if (c.Calculate(10, '-', 10) != 0) return false;
+        if (c.Calculate(10, '-', 10) != 0) return false;
    std:cout << "-Check multiplying " << std::endl;
-    if (c.Calculate(10, '*', 10) != 100) return false;
+        if (c.Calculate(10, '*', 10) != 100) return false;
     std::cout << "-Check division " << std::endl;
-    if (c.Calculate(10, '/', 10) != 1) return false;
+        if (c.Calculate(10, '/', 10) != 1) return false;
     return true;
 }
+
+int main(){
+    unitTest();
+    return 0;
+}    
