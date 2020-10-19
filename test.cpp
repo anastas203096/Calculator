3c8382beaeb38c657b7087bc4ca8acc3d02a03d0
Strelok-R <72858444+Strelok-R@users.noreply.github.com>
2020-10-19 16:13:56 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include "Calculator.h"
+#define CATCH_CONFIG_MAIN
+
+using namespace std;
+
+bool unitTest() {
+    Calculator c;
+
+    cout << "-Check sum " << endl;
+    if (c.Calculate(10, '+', 10) != 20) return false;
+    cout << "-Check substraction " << endl;
+    if (c.Calculate(10, '-', 10) != 0) return false;
+    cout << "-Check multiplying " << endl;
+    if (c.Calculate(10, '*', 10) != 100) return false;
+    cout << "-Check division " << endl;
+    if (c.Calculate(10, '/', 10) != 1) return false;
+    return true;
+}
